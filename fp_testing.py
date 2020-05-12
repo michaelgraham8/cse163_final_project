6bd35d67ad539dd92f4ff1cc9c28186bdf98e146
mgraham8 <mgraham8@uw.edu>
2020-05-11 22:09:02 -0500

    removed stuff

--- fp_testing.py
+++ fp_testing.py
@@ -1,13 +1,8 @@
-import sys
+
 import requests as re
 
-print(sys.version)
-print(sys.executable)
 
 
-def greet(who):
-    greeting = "Hello, {}".format(who)
-    return greeting
 
 
 r = re.get("https://coreyms.com")
